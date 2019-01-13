a992f1b6da6f035d0f94f78cd290a696bcbf2060
stan <momos.wang@gmail.com>
2019-01-13 13:09:59 +0800

    第一次上传代码
    学习git，上传一些练习py代码

---
+++ wordcount/function.py
@@ -0,0 +1,24 @@
+#from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+	return render(request, 'home.html')
+
+
+def count(request):
+	req_text = request.GET['text']
+	total_count = len(req_text)
+
+	word_dict = {}
+
+	for word in req_text:
+		if word not in word_dict:
+			word_dict[word] = 1
+		else:
+			word_dict[word] += 1
+
+	sortedword = sorted(word_dict.items(), key = lambda w:w[1], reverse = True )
+
+	return render(request, 'count.html', {'count': total_count, 'reqtext': req_text, 'wordict': word_dict , 'sortedword': sortedword})
+
